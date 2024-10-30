0a0e4c20fa1d4cff5c52b766f6a31e3d779599d5
Rakesh Chaudhary <9106985213r@gmail.com>
2024-10-30 14:58:53 +0530

    give images correct path

--- src/components/ProductData.js
+++ src/components/ProductData.js
@@ -4,84 +4,84 @@
         {
           name: 'Almond',
           price: 920,
-          img: '.images/almond2.jpg',
+          img: './images/almond2.jpg',
           tax: 20,
           id: 1
         },
         {
           name: 'Cashew',
           price: 980,
-          img: '.images/cashew-img.jpeg',
+          img: './images/cashew-img.jpeg',
           tax: 23,
           id: 2
         },
         {
           name: 'Walnut',
           price: 1200,
-          img: '.images/walnut-img.avif',
+          img: './images/walnut-img.avif',
           tax: 26,
           id: 3
         },
         {
           name: 'Dried Black Currants',
           price: 940,
-          img: '.images/black-currant-img2.png',
+          img: './images/black-currant-img2.png',
           tax: 20,
           id: 4
         },
         {
           name: 'Dried Cranberries',
           price: 1050,
-          img: '.images/Dried-Cranberries.jpg',
+          img: './images/Dried-Cranberries.jpg',
           tax: 23,
           id: 5
         },
          {
           name: 'Macadamia Nuts',
           price: 4275,
-          img: '.images/Macadamia-Nuts.jpg',
+          img: './images/Macadamia-Nuts.jpg',
           tax: 56,
           id: 6
         },
         {
           name: 'Hazelnuts',
           price: 1030,
-          img: '.images/Hazelnut.jpg',
+          img: './images/Hazelnut.jpg',
           tax: 22,
           id: 7
         },
         {
           name: 'Dried Figs',
           price: 730,
-          img: '.images/Dried-Figs.jpg',
+          img: './images/Dried-Figs.jpg',
           tax: 17,
           id: 8
         },
         {
           name: 'Raisins',
           price: 355,
-          img: '.images/Raisins.jpg',
+          img: './images/Raisins.jpg',
           tax: 14,
           id: 9
         },
         {
           name: 'Dried Apricots',
           price: 1350,
-          img: '.images/Dried-Apricots.jpg',
+          img: './images/Dried-Apricots.jpg',
           tax: 27,
           id: 10
         },
         {
           name: 'Pistachios',
           price: 1485,
-          img: '.images/Pistachios1.jpg',
+          img: './images/Pistachios1.jpg',
           tax: 29,
           id: 11
         },
         {
           name: 'Dried Kiwi',
           price: 1350,
-          img: '.images/Dried-Kiwi1.jpg',
+          img: './images/Dried-Kiwi1.jpg',
           tax: 25,
           id: 12
         },
@@ -90,84 +90,84 @@
         {
             name: 'Pecans',
             price: 1650,
-            img: '.images/Pecans.jpg',
+            img: './images/Pecans.jpg',
             tax: 27,
             id: 13
         },
         {
           name: 'Dried Dates',
           price: 370,
-          img: '.images/Dried-Dates.jpg',
+          img: './images/Dried-Dates.jpg',
           tax: 14,
           id: 14
         },
         {
           name: 'Brazil Nuts',
           price: 1780,
-          img: '.images/Brazil-Nuts.jpg',
+          img: './images/Brazil-Nuts.jpg',
           tax: 30,
           id: 15
         },
         {
           name: 'Chestnuts',
           price: 770,
-          img: '.images/Chestnuts1.jpg',
+          img: './images/Chestnuts1.jpg',
           tax: 18,
           id: 16
         },
         {
           name: 'Pine Nuts',
           price: 2999,
-          img: '.images/Pine-Nuts.jpg',
+          img: './images/Pine-Nuts.jpg',
           tax: 37,
           id: 17
         },
         {
           name: 'Tiger Nuts',
           price: 1200,
-          img: '.images/Tiger-Nuts.jpg',
+          img: './images/Tiger-Nuts.jpg',
           tax: 24,
           id: 18
         },
         {
           name: 'Dried Goji Berries',
           price: 2700,
-          img: '.images/Dried-Goji-Berries.jpg',
+          img: './images/Dried-Goji-Berries.jpg',
           tax: 34,
           id: 19
         },
         {
           name: 'Candlenuts',
           price: 800,
-          img: '.images/Candlenuts.jpg',
+          img: './images/Candlenuts.jpg',
           tax: 20,
           id: 20
         },
         {
           name: 'Dried Coconut Chips',
           price: 540,
-          img: '.images/Coconut-Chips.jpg',
+          img: './images/Coconut-Chips.jpg',
           tax: 16,
           id: 21
         },
         {
           name: 'Dried Cherries',
           price: 420,
-          img: '.images/Dried-Cherries.jpg',
+          img: './images/Dried-Cherries.jpg',
           tax: 14,
           id: 22
         },
         {
           name: 'Dried Mango',
           price: 320,
-          img: '.images/Dried-Mango.jpg',
+          img: './images/Dried-Mango.jpg',
           tax: 20,
           id: 23
         },
         {
           name: 'Peanuts',
           price: 240,
-          img: '.images/peanuts.jpg',
+          img: './images/peanuts.jpg',
           tax: 12,
           id: 24
         },
